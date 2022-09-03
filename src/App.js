1edf26e8a09f02b65ec0a569eb5c645f79785ac9
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2022-09-03 20:11:32 +0630

    change MyPortfolio

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="demo-big-content">
     <Layout>
-        <Header className='header-color' title={<Link style={{textDecoration:'none' , color:'white'}} to="/" >MyProtfolio</Link>} scroll>
+        <Header className='header-color' title={<Link style={{textDecoration:'none' , color:'white'}} to="/" >MyPortfolio</Link>} scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
@@ -16,7 +16,7 @@ function App() {
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title={<Link style={{textDecoration:'none' , color:'black'}} to="/" >MyProtfolio</Link>}>
+        <Drawer title={<Link style={{textDecoration:'none' , color:'black'}} to="/" >MyPortfolio</Link>}>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
