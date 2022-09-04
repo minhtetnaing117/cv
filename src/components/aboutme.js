35381c8733c535a157218c1317a2000de295f66f
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2022-09-04 16:59:13 +0630

    about me change 2 sen

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -8,13 +8,14 @@ class AboutMe extends Component {
                <h1>About Me</h1>
 
                <hr />
-
-               <p >
-               To work as <strong>Odoo developer </strong> in a company with world leading technology that provide the opportunities for continuous learning new things and to get a challenge role.<br/><br/>
-               I graduated with a Bachelor of Engineering degree in Mining Engineering from West Yangon Technology University (Hlaing Thar Yar).<br/><br/>After got my degree, worked as a Supervisor in Mining Company(National Property Company).<br/><br/>
-                    So, with about one year and 8 months experience in Mining and Management field. <br/><br/>That’s support experience and good attitude toward company workmates.<br/> <br/>I’m so quick learner in excelling and networking at new technologies as needed.<br/><br/>
-                    <strong>I had one and half experience for odoo developer.</strong>
-                    </p> 
+               <p>I graduated with a Bachelor of Engineering degree in Mining Engineering from West Yangon Technology University (Hlaing Thar Yar).</p>
+               <p>After received my Bachelor, National Property I had experiences for almost 2 years in Mining & Management field by working supervisor role at National Property Mining Company. I supervised over all workers and reported necessary issues to boss by daily.</p>
+               // <p >
+               // To work as <strong>Odoo developer </strong> in a company with world leading technology that provide the opportunities for continuous learning new things and to get a challenge role.<br/><br/>
+               // I graduated with a Bachelor of Engineering degree in Mining Engineering from West Yangon Technology University (Hlaing Thar Yar).<br/><br/>After got my degree, worked as a Supervisor in Mining Company(National Property Company).<br/><br/>
+               //      So, with about one year and 8 months experience in Mining and Management field. <br/><br/>That’s support experience and good attitude toward company workmates.<br/> <br/>I’m so quick learner in excelling and networking at new technologies as needed.<br/><br/>
+               //      <strong>I had one and half experience for odoo developer.</strong>
+               //      </p> 
                 
 
             </div>
