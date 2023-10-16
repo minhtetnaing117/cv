acca808f806f54f754a5462af4337a793b64edb7
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2023-10-16 23:00:02 +0630

    s
    s

--- src/components/personalDetail.js
+++ src/components/personalDetail.js
@@ -4,9 +4,9 @@ import { Cell, Grid } from 'react-mdl'
 class PersonalDetail extends Component {
     render() {
         return (
-            <div>
+            <div className="personal-detail-col">
                 <Grid>
-                <Cell col={4}>
+                <Cell  col={4}>
                         <p>{this.props.Gender}</p>
                         <p>{this.props.Date_of_Birth}</p>
                         <p>{this.props.Marital_status}</p>
@@ -15,8 +15,8 @@ class PersonalDetail extends Component {
                         <p>{this.props.Race}</p>
                         
                 </Cell>
-                <Cell col={8}>
-                <p style={{marginTop:'0px'}}>{this.props.GenderName}</p>
+                <Cell className="personal-detail-right" col={8}>
+                <p  style={{marginTop:'0px'}}>{this.props.GenderName}</p>
                           <p>{this.props.Birth}</p>
                           <p>{this.props.status}</p>
                           <p>{this.props.Nationallys}</p>
